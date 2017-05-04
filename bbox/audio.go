e744500004be3061edc1dc597ea5f5c36673fc32
Andrew Seigner <andrew@sig.gy>
2017-05-04 01:02:48 -0700

    moved playback code into wav.go

--- bbox/audio.go
+++ bbox/audio.go
@@ -1,13 +1,8 @@
 package bbox
 
 import (
-	"fmt"
-	"io"
 	"os"
 	"time"
-
-	"github.com/gordonklaus/portaudio"
-	"github.com/youpy/go-wav"
 )
 
 const (
@@ -15,91 +10,9 @@ const (
 	BEATS    = 4
 	TICKS    = 16
 	INTERVAL = 60 * time.Second / BPM / (TICKS / 4) // 4 beats per interval
-	BUF      = 16
-	MAX_BUF  = 524288
 )
 
 type Beats [BEATS][TICKS]bool
-
-var empty = make([]float32, BUF)
-
-type Wav struct {
-	name      string
-	buf       []float32
-	length    int
-	stream    *portaudio.Stream
-	active    chan struct{}
-	remaining int
-}
-
-func InitWav(f os.FileInfo) *Wav {
-	file, _ := os.Open("./wav/" + f.Name())
-	reader := wav.NewReader(file)
-	defer file.Close()
-
-	buf := [MAX_BUF]float32{}
-	loc := 0
-	for {
-		samples, err := reader.ReadSamples()
-		if err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				panic(err)
-			}
-		}
-
-		for _, sample := range samples {
-			buf[loc] = float32(reader.FloatValue(sample, 0))
-			loc += 1
-		}
-	}
-
-	w := Wav{
-		name:   f.Name(),
-		buf:    make([]float32, loc),
-		length: loc,
-		active: make(chan struct{}),
-	}
-	copy(w.buf, buf[:])
-
-	fmt.Printf("%+v: %+v samples\n", w.name, loc)
-
-	var err error
-	w.stream, err = portaudio.OpenDefaultStream(0, 1, 44100, BUF, w.cb)
-	if err != nil {
-		panic(err)
-	}
-	err = w.stream.Start()
-	if err != nil {
-		panic(err)
-	}
-
-	return &w
-}
-
-func (w *Wav) Close() {
-	w.stream.Stop()
-	w.stream.Close()
-}
-
-func (w *Wav) cb(output [][]float32) {
-	select {
-	case <-w.active:
-		w.remaining = w.length
-	default:
-	}
-
-	if w.remaining > 0 {
-		if w.remaining < BUF {
-			output[0] = output[0][:w.remaining]
-		}
-		copy(output[0], w.buf[(w.length-w.remaining):])
-		w.remaining -= BUF
-	} else {
-		copy(output[0], empty)
-	}
-}
 
 type Audio struct {
 	beats Beats
@@ -108,8 +21,6 @@ type Audio struct {
 }
 
 func InitAudio(msgs <-chan Beats, files []os.FileInfo) *Audio {
-	portaudio.Initialize()
-
 	a := Audio{
 		beats: Beats{},
 		msgs:  msgs,
@@ -144,7 +55,7 @@ func (a *Audio) Run() {
 			for i, beat := range a.beats {
 				if beat[cur] {
 					// initiate playback
-					go func(j int) { a.wavs[j].active <- struct{}{} }(i)
+					a.wavs[i].Play()
 				}
 			}
 			// next interval
---
+++ bbox/wav.go
@@ -0,0 +1,102 @@
+package bbox
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/gordonklaus/portaudio"
+	"github.com/youpy/go-wav"
+)
+
+const (
+	BUF     = 16
+	MAX_BUF = 524288
+)
+
+var empty = make([]float32, BUF)
+
+type Wav struct {
+	name      string
+	buf       []float32
+	length    int
+	stream    *portaudio.Stream
+	active    chan struct{}
+	remaining int
+}
+
+func InitWav(f os.FileInfo) *Wav {
+	portaudio.Initialize()
+
+	file, _ := os.Open("./wav/" + f.Name())
+	reader := wav.NewReader(file)
+	defer file.Close()
+
+	buf := [MAX_BUF]float32{}
+	loc := 0
+	for {
+		samples, err := reader.ReadSamples()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				panic(err)
+			}
+		}
+
+		for _, sample := range samples {
+			buf[loc] = float32(reader.FloatValue(sample, 0))
+			loc += 1
+		}
+	}
+
+	w := Wav{
+		name:   f.Name(),
+		buf:    make([]float32, loc),
+		length: loc,
+		active: make(chan struct{}),
+	}
+	copy(w.buf, buf[:])
+
+	fmt.Printf("%+v: %+v samples\n", w.name, loc)
+
+	var err error
+	w.stream, err = portaudio.OpenDefaultStream(0, 1, 44100, BUF, w.cb)
+	if err != nil {
+		panic(err)
+	}
+	err = w.stream.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	return &w
+}
+
+func (w *Wav) Play() {
+	w.active <- struct{}{}
+}
+
+func (w *Wav) Close() {
+	w.stream.Stop()
+	w.stream.Close()
+	portaudio.Terminate()
+}
+
+func (w *Wav) cb(output [][]float32) {
+	select {
+	case <-w.active:
+		w.remaining = w.length
+	default:
+	}
+
+	if w.remaining > 0 {
+		if w.remaining < BUF {
+			output[0] = output[0][:w.remaining]
+		}
+		copy(output[0], w.buf[(w.length-w.remaining):])
+		w.remaining -= BUF
+	} else {
+		copy(output[0], empty)
+	}
+}
