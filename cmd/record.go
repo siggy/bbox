001b5e5a49be53419570e6c06f2258116c044c5a
Andrew Seigner <andrew@sig.gy>
2017-07-31 21:00:57 -0700

    record.go cleanup

--- cmd/record.go
+++ cmd/record.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"github.com/gordonklaus/portaudio"
 	"math"
@@ -55,60 +54,10 @@ func median(slice []int32) float64 {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("missing required argument:  output file name")
-		return
-	}
-	fmt.Println("Recording.  Press Ctrl-C to stop.")
+	fmt.Println("Press Ctrl-C to stop.")
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
-
-	fileName := os.Args[1]
-	if !strings.HasSuffix(fileName, ".aiff") {
-		fileName += ".aiff"
-	}
-	f, err := os.Create(fileName)
-	chk(err)
-
-	// form chunk
-	_, err = f.WriteString("FORM")
-	chk(err)
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //total bytes
-	_, err = f.WriteString("AIFF")
-	chk(err)
-
-	// common chunk
-	_, err = f.WriteString("COMM")
-	chk(err)
-	chk(binary.Write(f, binary.BigEndian, int32(18)))                  //size
-	chk(binary.Write(f, binary.BigEndian, int16(1)))                   //channels
-	chk(binary.Write(f, binary.BigEndian, int32(0)))                   //number of samples
-	chk(binary.Write(f, binary.BigEndian, int16(32)))                  //bits per sample
-	_, err = f.Write([]byte{0x40, 0x0e, 0xac, 0x44, 0, 0, 0, 0, 0, 0}) //80-bit sample rate 44100
-	chk(err)
-
-	// sound chunk
-	_, err = f.WriteString("SSND")
-	chk(err)
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //size
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //offset
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //block
-	nSamples := 0
-	defer func() {
-		// fill in missing sizes
-		totalBytes := 4 + 8 + 18 + 8 + 8 + 4*nSamples
-		_, err = f.Seek(4, 0)
-		chk(err)
-		chk(binary.Write(f, binary.BigEndian, int32(totalBytes)))
-		_, err = f.Seek(22, 0)
-		chk(err)
-		chk(binary.Write(f, binary.BigEndian, int32(nSamples)))
-		_, err = f.Seek(42, 0)
-		chk(err)
-		chk(binary.Write(f, binary.BigEndian, int32(4*nSamples+8)))
-		chk(f.Close())
-	}()
 
 	portaudio.Initialize()
 	defer portaudio.Terminate()
@@ -119,20 +68,23 @@ func main() {
 
 	// -2147483648 to 2147483647
 	amp := float64(0)
+	last := float64(0)
 
 	chk(stream.Start())
 	for {
 		chk(stream.Read())
-		chk(binary.Write(f, binary.BigEndian, in))
-		nSamples += len(in)
 		// fmt.Println(in)
 		amp = math.Max(max(in), amp*0.95)
 		db := 20 * math.Log10(amp/math.MaxInt32)
-		fmt.Printf("\r%s", strings.Repeat(" ", 100))
+		// fmt.Printf("\r%s", strings.Repeat(" ", 100))
 		level := int(db + 50)
 		if level > 0 {
-			fmt.Printf("\r%s", strings.Repeat("#", level))
+			fmt.Printf("%s\n", strings.Repeat("#", level))
 		}
+		if last < db {
+			fmt.Printf("%s\n", db-last)
+		}
+		last = db
 
 		select {
 		case <-sig:
@@ -145,6 +97,6 @@ func main() {
 
 func chk(err error) {
 	if err != nil {
-		// panic(err)
+		panic(err)
 	}
 }
