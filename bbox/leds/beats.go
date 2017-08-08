acd1479f4667d28b534253f156a43cfd641ab382
Andrew Seigner <andrew@sig.gy>
2017-08-08 05:46:30 +0000

    led intervals wip

--- bbox/leds/beats.go
+++ bbox/leds/beats.go
@@ -2,6 +2,7 @@ package leds
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/siggy/bbox/bbox"
 	"github.com/siggy/rpi_ws281x/golang/ws2811"
@@ -9,11 +10,55 @@ import (
 
 const (
 	LED_COUNT  = 150
-	TICK_DELAY = 3 // match sound to LEDs
+	TICK_DELAY = 0 // match sound to LEDs
 )
+
+type Row struct {
+	start   int
+	end     int
+	buttons [bbox.BEATS]int
+}
+
+func (r *Row) TickToLed(tick int) int {
+	// determine where we are in the buttons array
+	// 0 <= tick < 160
+	// 0 <= beat < 16
+	floatBeat := float64(tick) / float64(bbox.TICKS_PER_BEAT) // 12.7 => 0.7
+	f := math.Floor(floatBeat)                                // 12
+	c := math.Ceil(floatBeat)                                 // 13
+
+	var floor int
+	var ceil int
+
+	if f == 0 {
+		// between start and first beat
+		floor = r.start
+		ceil = r.buttons[int(c)]
+	} else if c == bbox.BEATS {
+		// between last beat and end
+		floor = r.buttons[int(f)]
+		ceil = r.end
+	} else {
+		// between first and last beat
+		floor = r.buttons[int(f)]
+		ceil = r.buttons[int(c)]
+	}
+
+	percentAhead := floatBeat - f
+	diff := percentAhead * (float64(ceil) - float64(floor))
+	return floor + int(diff)
+}
+
+type LedBeats struct {
+	beats   bbox.Beats
+	closing chan struct{}
+	msgs    <-chan bbox.Beats
+	ticks   <-chan int
+}
 
 var (
 	rows = [bbox.SOUNDS]Row{
+		// rows 0 and 1 are LED strip 0
 		Row{
 			start: 33,
 			end:   0,
@@ -28,6 +73,8 @@ var (
 				36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66,
 			},
 		},
+
+		// rows 1 and 2 are LED strip 1
 		Row{
 			start: 43,
 			end:   0,
@@ -44,19 +91,6 @@ var (
 		},
 	}
 )
-
-type Row struct {
-	start   int
-	end     int
-	buttons [bbox.BEATS]int
-}
-
-type LedBeats struct {
-	beats   bbox.Beats
-	closing chan struct{}
-	msgs    <-chan bbox.Beats
-	ticks   <-chan int
-}
 
 func InitLedBeats(msgs <-chan bbox.Beats, ticks <-chan int) *LedBeats {
 	InitLeds(LED_COUNT, LED_COUNT)
@@ -99,18 +133,22 @@ func (l *LedBeats) Run() {
 			// TODO: leds for all 4 beats
 			tick = (tick + bbox.TICKS - TICK_DELAY) % bbox.TICKS
 			ws2811.Clear()
-			beat := tick / bbox.TICKS_PER_BEAT
+			// cur := tick / bbox.TICKS_PER_BEAT
 
-			for _, r := range rows {
-				ws2811.SetLed(0, r.buttons[beat], trueWhite)
-				ws2811.SetLed(1, r.buttons[beat], trueWhite)
+			// light all leds at current position
+			for _, r := range rows[0:2] {
+				ws2811.SetLed(0, r.TickToLed(tick), trueWhite)
+			}
+			for _, r := range rows[2:4] {
+				ws2811.SetLed(1, r.TickToLed(tick), trueWhite)
 			}
 
+			// light active beats
 			// for _, beat := range l.beats {
 			// 	for j, t := range beat {
 			// 		if t {
 			// 			for _, r := range rows {
-			// 				if j == tick {
+			// 				if j == cur {
 			// 					ws2811.SetLed(0, r.buttons[j], redWhite)
 			// 				} else {
 			// 					ws2811.SetLed(0, r.buttons[j], trueRed)
