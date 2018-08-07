00a0d1975237f6ea3657d4ce7490da341ccdced8
Andrew Seigner <andrew@sig.gy>
2018-08-06 19:01:45 -0700

    set front leds for baux

--- cmd/baux.go
+++ cmd/baux.go
@@ -7,6 +7,22 @@ import (
 	"github.com/siggy/bbox/bbox/leds"
 )
 
+// LEDS
+frontLeds = []int{
+	0,
+	29,
+	55,
+	79,
+	102,
+	124,
+	145,
+	165,
+	183,
+	200,
+	216,
+	230,
+}
+
 func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
