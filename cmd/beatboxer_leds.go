a292c76306f44265e1f73f7feb6d3d32af9686a9
Andrew Seigner <andrew@sig.gy>
2018-08-17 00:14:35 -0700

    disable web interface for bbox LEDs program

--- cmd/beatboxer_leds.go
+++ cmd/beatboxer_leds.go
@@ -16,7 +16,6 @@ import (
 	"github.com/siggy/bbox/beatboxer/programs/intro"
 	"github.com/siggy/bbox/beatboxer/render"
 	"github.com/siggy/bbox/beatboxer/render/led"
-	"github.com/siggy/bbox/beatboxer/render/web"
 )
 
 func main() {
@@ -42,7 +41,7 @@ func main() {
 
 	harness := beatboxer.InitHarness(
 		[]render.Renderer{
-			web.InitWeb(),
+			// web.InitWeb(),
 			// render.InitTerminal(),
 			led.InitLed(),
 		},
