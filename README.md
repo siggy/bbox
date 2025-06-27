bd7d8a5515fe28526de897cdd52002f9bf051dde
Andrew Seigner <andrew@sig.gy>
2025-06-28 00:33:51 +0100

    add devil, pyramid

--- README.md
+++ README.md
@@ -3,11 +3,9 @@
 - auto speed up to 480
 - pyramid song
 - consider diff'ing against last LED state in leds.go
-- consider secret codes to access nice, other songs, programs
 - pre-programmed beats, like queen songs
 - need amplitude input into the programs
 - generic interface for LED lighting
-- validation program for testing individual LEDs
 
 # BBox
 
--- cmd/bbox2/main.go
+++ cmd/bbox2/main.go
@@ -12,8 +12,10 @@ import (
 	"github.com/siggy/bbox/bbox2/leds"
 	"github.com/siggy/bbox/bbox2/program"
 	"github.com/siggy/bbox/bbox2/programs/beats"
+	"github.com/siggy/bbox/bbox2/programs/devil"
 	"github.com/siggy/bbox/bbox2/programs/ledtest"
 	"github.com/siggy/bbox/bbox2/programs/nice"
+	"github.com/siggy/bbox/bbox2/programs/pyramid"
 	"github.com/siggy/bbox/bbox2/wavs"
 	log "github.com/sirupsen/logrus"
 )
@@ -92,6 +94,8 @@ func main() {
 		{new: beats.New, code: nil, hidden: false},
 		{new: ledtest.New, code: nil, hidden: false},
 		{new: nice.New, code: []int{1, 2, 1, 0}, hidden: true},
+		{new: devil.New, code: []int{0, 9, 1, 7}, hidden: true},
+		{new: pyramid.New, code: []int{0, 6, 0, 4}, hidden: true},
 	}
 
 	cur := 0
