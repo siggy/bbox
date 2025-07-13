2d07b7bccbd0cfb6948593eb17f9aa5108c83a16
johnusher <yesjhon@gmail.com>
2025-07-13 11:57:38 +0200

    undoing commit to bbox2/main.go

--- cmd/bbox2/main.go
+++ cmd/bbox2/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"os"
 	"os/signal"
 	"path/filepath"
 	"slices"
-	"strings"
 	"syscall"
 
 	"github.com/siggy/bbox/bbox2/keyboard"
@@ -34,16 +32,16 @@ const (
 )
 
 var (
+	// should match scorpio/code.py
+	// StripLengths = []int{30, 30, 10, 10, 10, 10, 10, 10}
 	stripLengths = []int{144}
 )
 
 func main() {
-	logLevel := flag.String("log-level", "info", "set log level (debug, info, warn, error, fatal, panic)")
+	logLevel := flag.String("log-level", "debug", "set log level (debug, info, warn, error, fatal, panic)")
 	bboxKB := flag.Bool("bbox-keyboard", false, "enable beatboxer keyboard")
 	fakeLEDs := flag.Bool("fake-leds", false, "enable fake LEDs")
 	macDevice := flag.Bool("mac-device", false, "connect to scorpio from a macbook (default is Raspberry Pi)")
-	user := flag.String("user", "siggy-pi", "set user, eg siggy-pi, jhon-mac")
-	eqBands := flag.Int("L", 16, "number of frequency bands (lights) for the EQ display")
 	flag.Parse()
 
 	lvl, err := log.ParseLevel(*logLevel)
@@ -61,13 +59,8 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	var wavPath string
-	if *user != "jhon-mac" {
-		wavPath = filepath.Join(os.Getenv("HOME"), "code", "bbox", "wavs")
-	} else {
-		wavPath = filepath.Join(os.Getenv("HOME"), "Documents", "bbox", "wavs")
-	}
-
+	// init
+	wavPath := filepath.Join(os.Getenv("HOME"), "code", "bbox", "wavs")
 	wavs, err := wavs.New(wavPath)
 	if err != nil {
 		log.Fatalf("wavs.New failed: %v", err)
@@ -110,12 +103,15 @@ func main() {
 	cur := 0
 	progCtx, cancelProg := context.WithCancel(ctx)
 	curProgram := programs[cur].new(progCtx)
+
 	yieldCount := 0
+
 	rollingCode := []int{0, 0, 0, 0}
 
 	for {
 		yield := func(next program.ProgramFactory) {
 			log.Debugf("yield prev program: %s", curProgram.Name())
+
 			cancelProg()
 			curProgram.Close()
 
@@ -126,16 +122,20 @@ func main() {
 						break
 					}
 				}
+
 				next = programs[cur].new
 			} else {
+				// back next up one so we yield back to the same place
 				cur = (cur - 1 + len(programs)) % len(programs)
 			}
 
 			progCtx, cancelProg = context.WithCancel(ctx)
 			curProgram = next(progCtx)
+
 			ledStrips.Clear()
 			wavs.StopAll()
 			yieldCount = 0
+
 			log.Debugf("yield new program: %s", curProgram.Name())
 		}
 
@@ -143,13 +143,18 @@ func main() {
 		case press, ok := <-presses:
 			if !ok {
 				log.Info("keyboard channel closed, exiting...")
+
 				cancelProg()
 				curProgram.Close()
 				return
 			}
+
 			log.Debugf("press: %q", press)
+
+			// TODO: combine these two code patterns?
 			if press.Col == program.Cols-1 && press.Row == program.Rows-1 {
 				yieldCount++
+				log.Debugf("yieldCount: %d", yieldCount)
 				if yieldCount >= yieldLimit {
 					yield(nil)
 					continue
@@ -157,41 +162,37 @@ func main() {
 			} else {
 				yieldCount = 0
 			}
+
 			if press.Row == 0 {
 				rollingCode = append(rollingCode[1:], press.Col)
+
 				var found program.ProgramFactory
+
 				for _, p := range programs {
 					if slices.Equal(p.code, rollingCode) {
+						log.Debugf("rolling code matched: %v", rollingCode)
 						found = p.new
 						break
 					}
 				}
+
 				if found != nil {
 					yield(found)
 				}
 			} else {
 				rollingCode = []int{0, 0, 0, 0}
 			}
+
 			curProgram.Press(press)
 
 		case leds := <-curProgram.Render():
-			//old style led:
 			log.Tracef("leds: %s", leds)
-			ledStrips.Set(leds)
 
-			select {
-			case eqBandData := <-wavs.EQ():
-				// show color bar:
-				if len(eqBandData) == *eqBands {
-					eqLine := buildEqLine(eqBandData, *eqBands)
-					fmt.Println(eqLine)
-				}
-			default:
-				// No new EQ data, do nothing.
-			}
+			ledStrips.Set(leds)
 
 		case play := <-curProgram.Play():
 			log.Tracef("play: %s", play)
+
 			wavs.Play(play)
 
 		case <-curProgram.Yield():
@@ -199,45 +200,10 @@ func main() {
 
 		case <-ctx.Done():
 			log.Info("context done")
+
 			cancelProg()
 			curProgram.Close()
 			return
 		}
 	}
-}
-
-// --- Display Functions ---
-
-func magnitudeToColor(intensity float64) string {
-	if intensity < 0 {
-		intensity = 0
-	}
-	if intensity > 1 {
-		intensity = 1
-	}
-	r := byte(intensity * 255)
-	b := byte((1 - intensity) * 255)
-	return fmt.Sprintf("\033[38;2;%d;0;%dm", r, b)
-}
-
-
-// show line of 16 blocks based on EQ data
-// red is high intensity, blue is low intensity:
-func buildEqLine(eqData []float64, eqBands int) string {
-	var sb strings.Builder
-	const resetColor = "\033[0m"
-
-	sb.WriteString("EQ: [")
-	for i := 0; i < eqBands; i++ {
-		intensity := 0.0
-		if i < len(eqData) {
-			intensity = eqData[i]
-		}
-		sb.WriteString(magnitudeToColor(intensity))
-		sb.WriteString("█")
-		sb.WriteString(resetColor)
-	}
-	sb.WriteString("]")
-
-	return sb.String()
 }
