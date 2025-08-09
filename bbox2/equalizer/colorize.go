7f50eadb5d4913094d9c3a444eca8a41aa706d1c
Andrew Seigner <andrew@sig.gy>
2025-08-09 12:38:31 -0700

    introduce gamma curve to compress song colors

--- bbox2/equalizer/colorize.go
+++ bbox2/equalizer/colorize.go
@@ -9,6 +9,20 @@ import (
 // colorizer is a function type that maps a value from 0.0-1.0 to an LED color
 type colorizer func(float64) leds.Color
 
+const gamma = 6
+
+var lutGammaMap = lutGamma(gamma)
+
+func lutGamma(gamma float64) [256]uint8 {
+	var lut [256]uint8
+	inv := 1.0 / 255.0
+	for x := range lut {
+		y := 255 * math.Pow(float64(x)*inv, gamma)
+		lut[x] = uint8(math.Round(y))
+	}
+	return lut
+}
+
 // Colorize constructs the new 4-bar spectrum history dashboard.
 func Colorize(data DisplayData) [HistorySize][]leds.Color {
 	// This exponent will be used to create a curve, making low values even lower.
@@ -18,25 +32,25 @@ func Colorize(data DisplayData) [HistorySize][]leds.Color {
 		// 1. Electric Blue (Oldest)
 		func(val float64) leds.Color {
 			scaledVal := math.Pow(val, exponent)
-			r, g, b := uint8(0*scaledVal), uint8(200*scaledVal), uint8(255*scaledVal)
+			r, g, b := lutGammaMap[uint8(0*scaledVal)], lutGammaMap[uint8(200*scaledVal)], lutGammaMap[uint8(255*scaledVal)]
 			return leds.Color{R: r, G: g, B: b}
 		},
 		// 2. Toxic Green
 		func(val float64) leds.Color {
 			scaledVal := math.Pow(val, exponent)
-			r, g, b := uint8(100*scaledVal), uint8(255*scaledVal), uint8(100*scaledVal)
+			r, g, b := lutGammaMap[uint8(100*scaledVal)], lutGammaMap[uint8(255*scaledVal)], lutGammaMap[uint8(100*scaledVal)]
 			return leds.Color{R: r, G: g, B: b}
 		},
 		// 3. Sunset Orange
 		func(val float64) leds.Color {
 			scaledVal := math.Pow(val, exponent)
-			r, g, b := uint8(255*scaledVal), uint8(150*scaledVal), uint8(20*scaledVal)
+			r, g, b := lutGammaMap[uint8(255*scaledVal)], lutGammaMap[uint8(150*scaledVal)], lutGammaMap[uint8(20*scaledVal)]
 			return leds.Color{R: r, G: g, B: b}
 		},
 		// 4. Cyberpunk Pink (Newest)
 		func(val float64) leds.Color {
 			scaledVal := math.Pow(val, exponent)
-			r, g, b := uint8(255*scaledVal), uint8(50*scaledVal), uint8(200*scaledVal)
+			r, g, b := lutGammaMap[uint8(255*scaledVal)], lutGammaMap[uint8(50*scaledVal)], lutGammaMap[uint8(200*scaledVal)]
 			return leds.Color{R: r, G: g, B: b}
 		},
 	}
